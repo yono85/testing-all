d795b5a98fe5cf738d85034a145eab9f4079139f
yono85 <yonocahyono85@gmail.com>
2022-11-10 21:58:10 +0700

    first

--- kirim.php
+++ kirim.php
@@ -1,89 +1,47 @@
 <?php
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-// Load Composer's autoloader
+//Load Composer's autoloader
 require 'vendor/autoload.php';
 
-// use PHPMailer\PHPMailer\PHPMailer;
-// use PHPMailer\PHPMailer\Exception;
-
-// require 'vendor/PHPMailer/src/Exception.php';
-// require 'vendor/PHPMailer/src/PHPMailer.php';
-// require 'vendor/PHPMailer/src/SMTP.php';
-
+//Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
-try{
 
-    $mail->isSMTP();                                            
-    $mail->Host       = 'smtpmail.jakarta.go.id';  
-    $mail->SMTPAuth   = true;                     
-    $mail->Username   = 'noreply-sims@jakarta.go.id';
-    $mail->Password   = 'Sims@Dki2022!'; 
-    $mail->Port       = 587;
-    
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtpmail.jakarta.go.id';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'noreply-sims@jakarta.go.id';                     //SMTP username
+    $mail->Password   = 'Sims@Dki2022!';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
     //Recipients
-    $mail->setFrom('noreply-sims@jakarta.go.id', 'Testing Percobaan');
-    $mail->addAddress('yonocahyono85@gmail.com', 'Yono');     // Add a recipient
-    
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Testing Native PHP';
-    $mail->Body    = '<h4>Testing</h4><br/>Testing send email';    
+    $mail->setFrom('noreply-sims@jakarta.go.id', 'Testing');
+    $mail->addAddress('yonocahyono85@gmail.com', 'Yono Cahyono');     //Add a recipient
+    // $mail->addAddress('ellen@example.com');               //Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
     $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-catch(Exception $e){
-    dd($e);
-}
-
-
-// if(isset($_POST['submit']))
-// {
-//     // Instantiation and passing `true` enables exceptions
-//     $mail = new PHPMailer(true);
-
-//     $no_invoice         = $_POST['no_invoice'];
-//     $nama_pengirim      = $_POST['nama_pengirim'];
-//     $email              = $_POST['email'];
-
-//     //Server settings
-//     // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-//     $mail->isSMTP();                                            // Send using SMTP
-//     $mail->Host       = 'smtpmail.jakarta.go.id';                    // Set the SMTP server to send through
-//     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-//     $mail->Username   = 'noreply-sims@jakarta.go.id';                     // SMTP username
-//     $mail->Password   = 'Sims@Dki2022!';                               // SMTP password
-//     // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-//     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-//     //Recipients
-//     $mail->setFrom('noreply-sims@jakarta.go.id', 'Testing Percobaan');
-//     $mail->addAddress($email, $nama_pengirim);     // Add a recipient
-    
-//     // $mail->addReplyTo('namaemail', 'Percobaan');
-//     // $mail->addCC('cc@example.com');
-//     // $mail->addBCC('bcc@example.com');
-
-//     // Attachments
-//     // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//     // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-
-//     // Content
-//     $mail->isHTML(true);                                  // Set email format to HTML
-//     $mail->Subject = 'Testing Native PHP';
-//     $mail->Body    = '<h4>Testing</h4><br/>Testing send email';    
-
-//     if($mail->send())
-//     {
-//         echo 'Konfirmasi pembayaran telah berhasil';
-//     }
-//     else{
-//         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-//     }
-// }
-// else{
-//     echo "Tekan dulu tombolnya bos";
-// }
